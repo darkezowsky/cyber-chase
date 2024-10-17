95f0af0839bf07c4808108e7d8b4f2214a1be7d9
darkezowsky <darkezowsky@gmail.com>
2024-10-17 19:34:19 +0200

    Plugins update + bugs fixes

--- Assets/Packs/Standard Assets/Utility/EventSystemChecker.cs
+++ Assets/Packs/Standard Assets/Utility/EventSystemChecker.cs
@@ -5,17 +5,14 @@ using UnityEngine.EventSystems;
 
 public class EventSystemChecker : MonoBehaviour
 {
-    //public GameObject eventSystem;
-
-	// Use this for initialization
-	void Awake ()
-	{
-	    if(!FindObjectOfType<EventSystem>())
+    // Use this for initialization
+    void Awake()
+    {
+        if (!FindObjectOfType<EventSystem>())
         {
-           //Instantiate(eventSystem);
             GameObject obj = new GameObject("EventSystem");
             obj.AddComponent<EventSystem>();
-            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
+            obj.AddComponent<StandaloneInputModule>(); // Removed forceModuleActive
         }
-	}
+    }
 }
