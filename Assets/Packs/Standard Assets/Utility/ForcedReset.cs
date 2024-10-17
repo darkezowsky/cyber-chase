7f967cd43b283de6734306cc18dfa8af386896a8
darkezowsky <darkezowsky@gmail.com>
2024-10-17 14:44:21 +0200

    Engine update + Bug fixes

--- Assets/Packs/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Packs/Standard Assets/Utility/ForcedReset.cs
@@ -1,11 +1,18 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Dodano do u¿ycia UI.Image
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
 public class ForcedReset : MonoBehaviour
 {
+    private Image guiImage; // Zmienna do przechowywania referencji do Image
+
+    private void Awake()
+    {
+        guiImage = GetComponent<Image>(); // Uzyskujemy komponent Image
+    }
+
     private void Update()
     {
         // if we have forced a reset ...
